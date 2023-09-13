278eb1db6278bedf102abe2ca9693358cd0e6111
Felipe Fernandez Cronenbold <ffernandezcronenbol@mail.austral.edu.ar>
2023-09-12 23:37:51 -0300

    implemented Newton Interpolator(): NOT WORKINGgit add .git add .

--- numericCalculus.py
+++ numericCalculus.py
@@ -31,6 +31,18 @@ class Interpolation:
                 self.points.remove(self.points[i])
         raise ValueError("Not Found")
 
+    def show(self, interpolator: Callable[[int], int]) -> None:
+        x_values = np.linspace(self.minX, self.maxX, int(self.maxX - self.minX) * 100)
+        y_values = [interpolator(x) for x in x_values]
+
+        plt.plot(x_values, y_values)
+        plt.scatter([point[0] for point in self.points], [point[1] for point in self.points], color='red', marker='o')
+        plt.xlabel('x')
+        plt.ylabel('Interpolated Value')
+        plt.title(f"{self.__class__.__name__} interpolation")
+        plt.grid(True)
+        plt.show()
+
 
 class Lagrange(Interpolation):
     def __init__(self):
@@ -50,35 +62,43 @@ class Lagrange(Interpolation):
 
         return function
 
-    def show(self) -> None:
-        x_values = np.linspace(self.minX, self.maxX, int(self.maxX - self.minX) * 100)
-        y_values = [self.interpolate()(x) for x in x_values]
 
-        plt.plot(x_values, y_values)
-        plt.scatter([point[0] for point in self.points], [point[1] for point in self.points], color='red', marker='o')
-        plt.xlabel('x')
-        plt.ylabel('Interpolated Value')
-        plt.title("LAGRANGE INTERPOLATION")
-        plt.grid(True)
-        plt.show()
+class Newton(Interpolation):
+    def __init__(self):
+        super().__init__()
+        return
+
+    def multiplicatory(self, j, x):
+        return x - j
+
+    def interpolate(self) -> Callable[[int], int]:
+        def function(x: int) -> int:
+            interpolation_sum: int = 0
+            for i in range(len(self.points)):
+                Q: int = 1
+                for j in range(i):
+                    Q *= self.multiplicatory(self.points[j][0], self.points[i][0])
+                c: float = (self.points[i][1] - interpolation_sum) / Q
+                for j in range(i):
+                    Q *= self.multiplicatory(self.points[j][0], x)
+                    interpolation_sum += c * Q
+            return interpolation_sum
+
+        return function
 
 
 if __name__ == '__main__':
-    '''interpolator = Lagrange()
+    interpolator_1: Interpolation = Lagrange()
+    interpolator_2: Interpolation = Newton()
+    interpolators: List[Interpolation] = [interpolator_1, interpolator_2]
+
     while True:
         var0 = int(input("write x: "))
         var1 = int(input("write y: "))
-        interpolator.add_point(var0, var1)
+        for interpolator in interpolators:
+            interpolator.add_point(var0, var1)
         end = input("do you want to end \n [Y]     [N]: ")
         if end == 'Y':
             break
-
-    interpolator.show()'''
-    interpolator = Lagrange()
-
-    test_points = [(-2, 14), (-1, 6), (0, 3), (1, 2), (2, 15)]
-
-    for point in test_points:
-        interpolator.add_point(point[0], point[1])
-
-    interpolator.show()
+    for interpolator in interpolators:
+        interpolator.show(interpolator.interpolate())
